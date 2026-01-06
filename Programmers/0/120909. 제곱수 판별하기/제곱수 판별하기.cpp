e935308f7fa5cd3b465597938131bbd342f6ac5d
김찬혁 <121753386+ChanHyuc@users.noreply.github.com>
2026-01-06 16:38:02 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ Programmers/0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 2;
+    int num = 1;
+    
+    while(num * num <= n) {
+        if(num * num == n) {
+            answer = 1;
+            break;
+        }
+        num++;
+    }
+    
+    return answer;
+}
