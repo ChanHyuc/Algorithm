e190c35d68dcb669dfebbf92d9098957a52aea40
김찬혁 <121753386+ChanHyuc@users.noreply.github.com>
2026-03-22 18:20:07 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    
+    while(n != 1) {
+        for(int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                n = n / i;
+                answer.push_back(i);
+                break;
+            }
+        }
+    }
+    
+    sort(answer.begin(), answer.end());
+    answer.erase(unique(answer.begin(),answer.end()), answer.end());
+    
+    return answer;
+}
