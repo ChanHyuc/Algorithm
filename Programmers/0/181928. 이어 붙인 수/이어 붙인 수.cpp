3091198ca4f16cb85797d5cf3ecfdb41529762a5
ChanHyeok <121753386+ChanHyuc@users.noreply.github.com>
2026-03-25 18:34:08 +0900

    [level 0] Title: 이어 붙인 수

---
+++ Programmers/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    string odd = "";
+    string even = "";
+    
+    for(auto i: num_list) {
+        if(i % 2 == 0) {
+            odd += to_string(i);
+        } else {
+            even += to_string(i);
+        }
+    }
+    
+    return stoi(odd) + stoi(even);
+}
