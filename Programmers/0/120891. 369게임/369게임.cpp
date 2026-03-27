fac5e6f3df5c062bf24d18308273224060857624
ChanHyeok <121753386+ChanHyuc@users.noreply.github.com>
2026-03-27 09:59:09 +0900

    [level 0] Title: 369게임

---
+++ Programmers/0/120891. 369게임/369게임.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    string temp = to_string(order);
+    
+    for(auto i: temp) {
+        if(i == '3' || i == '6' || i == '9') answer++;
+    }
+    
+    return answer;
+}
