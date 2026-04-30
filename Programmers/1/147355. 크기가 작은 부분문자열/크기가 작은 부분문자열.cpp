6d7f1f3278cdc48d3f3b21d72a6421f3f666463f
ChanHyeok <121753386+ChanHyuc@users.noreply.github.com>
2026-04-30 21:04:00 +0900

    [level 1] Title: 크기가 작은 부분문자열

---
+++ Programmers/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string t, string p) {
+    int answer = 0;
+    
+    for(int i = 0; i < t.size() - p.size() + 1; i++) {
+        string sliceNumber = t.substr(i, p.size());
+        
+        if( stoll(sliceNumber) <= stoll(p) ) answer++;
+    }
+    
+    return answer;
+}
